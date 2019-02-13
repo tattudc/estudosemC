f7683628d7bdf3f30ff76c3c395c7f39d369c8c2
tattudc <tarcisiodantasrn@gmail.com>
2019-02-13 20:40:56 -0200

    continuação dos estudos
    Estudos de tipos de dados em C

---
+++ C/Programação em C da Udemy/Tipos de dados/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*char meuChar = 'T';
+    scanf("%c", &meuChar);
+    printf("%c\n", meuChar);
+
+    int meuInt = 2, meuInt2 = 3;
+    scanf("%i", &meuInt);
+    printf("Resultado: %i\n", meuInt);
+
+    float meuFloat;
+    scanf("%f", &meuFloat);
+    printf("Resultado: %.2f\n", meuFloat);
+
+    int num1, num2, soma;
+    printf("Informe numero 1: \n");
+    scanf("%i", &num1);
+    printf("Informe numero 2: \n");
+    scanf("%i", &num2);
+    soma = num1 + num2;
+    printf("Resultado: %i\n", soma);*/
+
+
+    system("pause");
+    return 0;
+}
