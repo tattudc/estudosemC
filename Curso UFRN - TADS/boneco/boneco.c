e96fdc054f66cb085e573e60a933bc4137cec652
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-02-25 08:58:14 -0300

    Boneco
    boneco simples feito em C

---
+++ Curso UFRN - TADS/boneco/boneco.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void desenhaBoneco(){
+	printf("\t\t\t 0   \n");
+	printf("\t\t\t/|\\   \n");
+	printf("\t\t\t/ \\  \n");
+}
+
+int main(){
+	desenhaBoneco();
+	printf("------------------------------------------------------------------------------\n");
+	system("pause");
+	return 0;
+}
