fc4e008527227f238f5704ed361bae0e1d87b28b
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-04-24 11:29:59 -0300

    Concluindo Exercicio 4

--- Curso UFRN - TADS/Lista 5 - Arrays/Exercicio 4/exercicio4.c
+++ Curso UFRN - TADS/Lista 5 - Arrays/Exercicio 4/exercicio4.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
+	srand(time(NULL));
+	int contador[6] = {}, num, i;
+	
+	//Entrada de dados
+	for(i = 0; i < 10; i++){
+		num = rand()%6 + 1;
+		printf("\nDADO: %d\n", num);
+		contador[num - 1]++;
+	}
+	printf("\nFACE\tREPETICOES\n");
+	for(i = 0; i < 6; i++){
+		printf("%d > %d\n",i+1, contador[i]);
+	}
+		
+	
     return 0;
 }
 
-/*  */
+/* Faça um programa que receba o número sorteado por um dado em 10 jogadas,
+mostre os números dos dados e a frequência com que apareceram. */
