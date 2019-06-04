08952129a372045028c676d660ae62d08f8131f4
Tarcisio Câmara <41845115+tattudc@users.noreply.github.com>
2019-06-04 11:52:32 -0300

    Dando um UP no joguin
    adicionando coisas

--- Curso UFRN - TADS/Game Urban Dead/UrbanDead.c
+++ Curso UFRN - TADS/Game Urban Dead/UrbanDead.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void telaInicial();
+struct personagem{
+  char personName[50];
+  int forca, dex, vita, PA, PD, PV, XP, NV;
+};
+
 int main(){
-int menu, personAtr[3];
-char personName[10];
+int menu, pntsdistri = 5, distri, distriEscol;
+struct personagem person;
 
 do{
-	printf("----------------------------------------------------------\n");
-	printf("----------------------URBAN DEAD--------------------------\n");
-	printf("----------------------------------------------------------\n");
+	telaInicial();
 	printf("Digite 1 para Novo Jogo\nDigite 2 para load game\nDigite 3 para sair\n");
 	scanf("%i", &menu);
 	
 	if(menu == 1){
-		//C�digo Jogo
+		//Código Jogo
 		system("cls");
 		printf("Bem vindo ao URBAN DEAD\nDigite o nome do seu personagem\n");
-		scanf("%s", &personName);
-		printf("Seu nome e %s, confere?\n", personName);
+    setbuf(stdin, NULL);
+		fgets(person.personName, 50, stdin);
+		printf("Seu nome e %s, confere?\n", person.personName);
+    printf("Vamos distribuir os pontos do seu char\nVoce tem 5 pontos para distribuir entre 3 atributos\nForca, Dextreza e Vitalidade\n");
+    person.forca = 0;
+    person.dex = 0;
+    person.vita = 0;
+    do{
+      printf("Quer distribuir quantos pontos?\n");
+      scanf("%i", &distri);
+      pntsdistri = pntsdistri - distri;
+      printf("Em qual atributo?\n1 - Forca\n2 - Dextreza\n3 - Vitalidade\n");
+      scanf("%i", &distriEscol);
+      if(distriEscol == 1){
+        person.forca = person.forca + distri;
+      }
+      else if(distriEscol == 2){
+        person.dex = person.dex + distri;
+      }
+      else if(distriEscol == 3){
+        person.vita = person.vita + distri;
+      }
+      else{
+        printf("Escolha incorreta\nDigite uma alternativa válida\n");
+        scanf("%i", &distriEscol);
+      }
+    }while(pntsdistri != 0);
+    person.PA = person.forca + person.dex;
+    person.PD = person.dex + person.vita;
+    person.PV = person.vita * 5;
+    person.NV = 1;
+    person.XP = 0;
+    printf("Nome: %s; Nivel = %i; XP = %i\n", person.personName, person.NV, person.XP);
+    printf("Forca: %i; Dex = %i; Vita = %i\nPA = %i; PD = %i; PV = %i\n", person.forca, person.dex, person.vita, person.PA, person.PD, person.PV);
 	}
 	else if(menu == 2){
 		//Load dos chars do BD
@@ -37,3 +73,9 @@ do{
 system("pause");
 return 0;
 }
+
+void telaInicial(){
+  printf("----------------------------------------------------------\n");
+  printf("----------------------URBANDEAD--------------------------\n");
+  printf("----------------------------------------------------------\n");
+};
